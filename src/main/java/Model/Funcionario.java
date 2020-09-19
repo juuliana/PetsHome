94cb1e1c835911a5f3945924c5884a7d7aae1cff
indominuShot <luang193@gmail.com>
2020-09-19 16:55:25 -0300

    Model Funcionario adicionado

---
+++ src/main/java/Model/Funcionario.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author luang
+ */
+public class Funcionario {
+
+    /**
+     * @return the func_id
+     */
+    public int getFunc_id() {
+        return func_id;
+    }
+
+    /**
+     * @param func_id the func_id to set
+     */
+    public void setFunc_id(int func_id) {
+        this.func_id = func_id;
+    }
+
+    /**
+     * @return the func_nome
+     */
+    public String getFunc_nome() {
+        return func_nome;
+    }
+
+    /**
+     * @param func_nome the func_nome to set
+     */
+    public void setFunc_nome(String func_nome) {
+        this.func_nome = func_nome;
+    }
+
+    /**
+     * @return the func_end
+     */
+    public String getFunc_end() {
+        return func_end;
+    }
+
+    /**
+     * @param func_end the func_end to set
+     */
+    public void setFunc_end(String func_end) {
+        this.func_end = func_end;
+    }
+
+    /**
+     * @return the func_contato
+     */
+    public String getFunc_contato() {
+        return func_contato;
+    }
+
+    /**
+     * @param func_contato the func_contato to set
+     */
+    public void setFunc_contato(String func_contato) {
+        this.func_contato = func_contato;
+    }
+
+    /**
+     * @return the func_cpf
+     */
+    public String getFunc_cpf() {
+        return func_cpf;
+    }
+
+    /**
+     * @param func_cpf the func_cpf to set
+     */
+    public void setFunc_cpf(String func_cpf) {
+        this.func_cpf = func_cpf;
+    }
+
+    /**
+     * @return the func_email
+     */
+    public String getFunc_email() {
+        return func_email;
+    }
+
+    /**
+     * @param func_email the func_email to set
+     */
+    public void setFunc_email(String func_email) {
+        this.func_email = func_email;
+    }
+
+    /**
+     * @return the func_senha
+     */
+    public String getFunc_senha() {
+        return func_senha;
+    }
+
+    /**
+     * @param func_senha the func_senha to set
+     */
+    public void setFunc_senha(String func_senha) {
+        this.func_senha = func_senha;
+    }
+    
+     private int func_id;
+    private String func_nome;
+    private String func_end;
+    private String func_contato;
+    private String func_cpf;
+    private String func_email;
+    private String func_senha;
+    
+}
