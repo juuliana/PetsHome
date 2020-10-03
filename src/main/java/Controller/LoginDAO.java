fe76ee0d773bf925c0d8a209f9d5bb53610b0067
juuliana <jumaju10@hotmail.com>
2020-10-03 19:31:01 -0300

    update -images

--- src/main/java/Controller/LoginDAO.java
+++ src/main/java/Controller/LoginDAO.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Controller;
 
 import Model.Funcionario;
@@ -13,10 +8,7 @@ import java.sql.ResultSet;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author luang
- */
+
 public class LoginDAO {
     Connection conn = null;
     PreparedStatement ps = null;
