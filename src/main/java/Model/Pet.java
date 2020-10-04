f5503a957ba77b3ee0a2e0d0ea7be228cf7e9ff5
indominuShot <luang193@gmail.com>
2020-10-04 17:38:18 -0300

    Model pet adicionado

---
+++ src/main/java/Model/Pet.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author luang
+ */
+public class Pet {
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the raca
+     */
+    public String getRaca() {
+        return raca;
+    }
+
+    /**
+     * @param raca the raca to set
+     */
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    /**
+     * @return the porte
+     */
+    public String getPorte() {
+        return porte;
+    }
+
+    /**
+     * @param porte the porte to set
+     */
+    public void setPorte(String porte) {
+        this.porte = porte;
+    }
+
+    /**
+     * @return the idade
+     */
+    public int getIdade() {
+        return idade;
+    }
+
+    /**
+     * @param idade the idade to set
+     */
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    /**
+     * @return the dono
+     */
+    public int getDono() {
+        return dono;
+    }
+
+    /**
+     * @param dono the dono to set
+     */
+    public void setDono(int dono) {
+        this.dono = dono;
+    }
+    
+    private int id;
+    private String nome;
+    private String raca;
+    private String porte;
+    private int idade;
+    private int dono;    
+}
