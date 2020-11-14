9ce4bdc08d8b1bea8d467a2ce8243de0e373a8af
juuliana <jumaju10@hotmail.com>
2020-11-14 15:26:31 -0300

    FuncAgendamento -add

---
+++ src/main/java/Functions/FuncAgendamento.java
@@ -0,0 +1,121 @@
+package Functions;
+
+import Model.Agendamento;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+
+public class FuncAgendamento {
+
+    public void limparCampos(JTextField id,  JTextField data, JTextField hora, JComboBox tipo, JTextField valor, JTextField pet, JTextField cliente) {
+        id.setText(null);
+        data.setText(null);
+        hora.setText(null);
+        tipo.setSelectedIndex(1);
+        valor.setText(null);
+        pet.setText(null);
+        cliente.setText(null);
+        
+        Agendamento ag = new Agendamento();
+        
+        ag.setIsUpdating(false);
+    }
+
+    public boolean verificarCampos(JTextField txt_data, JTextField txt_hora, JComboBox box_tipo, JTextField txt_pet, JFrame jfagendamento) {
+
+        if (txt_data.getText().isEmpty()) {
+            JOptionPane.showMessageDialog(jfagendamento, "Por favor, preencha o campo Data");
+            txt_data.requestFocus();
+            return false;
+        } else {
+            if(txt_hora.getText().isEmpty()){
+                JOptionPane.showMessageDialog(jfagendamento, "Por favor, preencha o campo Hora");
+                txt_hora.requestFocus();
+                return false;
+            }else{
+                if(box_tipo.getSelectedItem().toString().isEmpty()){
+                    JOptionPane.showMessageDialog(jfagendamento, "Por favor, preencha o campo Tipo");
+                    box_tipo.requestFocus();
+                    return false;
+                }else{
+                    if(txt_pet.getText().isEmpty()){
+                        JOptionPane.showMessageDialog(jfagendamento, "Por favor, preencha o campo Pet");
+                        txt_pet.requestFocus();
+                        return false;
+                    }
+                }
+            }
+        }
+        
+        return true;
+    }
+   
+    public void enviarDados(JTable tabAgendamento, JTextField id,  JTextField data, JTextField hora, JComboBox tipo, JTextField valor, JTextField pet, JTextField cliente, JTabbedPane pagina){  
+        int linha = tabAgendamento.getSelectedRow();
+        
+        id.setText(tabAgendamento.getModel().getValueAt(linha, 0).toString());
+        data.setText(tabAgendamento.getModel().getValueAt(linha, 1).toString());
+        hora.setText(tabAgendamento.getModel().getValueAt(linha, 2).toString());
+        tipo.setSelectedItem(tabAgendamento.getModel().getValueAt(linha, 3).toString());
+        valor.setText(tabAgendamento.getModel().getValueAt(linha, 4).toString());
+        pet.setText(tabAgendamento.getModel().getValueAt(linha, 5).toString());
+        cliente.setText(tabAgendamento.getModel().getValueAt(linha, 5).toString());
+        
+        pagina.setSelectedIndex(1);  
+    }
+
+    public void habilitarCampos(JTextField id,  JTextField data, JTextField hora, JComboBox tipo, JTextField valor, JTextField pet, JTextField cliente) {
+
+        id.setEnabled(true);
+        data.setEnabled(true);
+        hora.setEnabled(true);
+        tipo.setEnabled(true);
+        valor.setEnabled(true);
+        pet.setEnabled(true);
+        cliente.setEnabled(true);
+    }
+
+    public void desabilitarCampos(JTextField id,  JTextField data, JTextField hora, JComboBox tipo, JTextField valor, JTextField pet, JTextField cliente) {
+
+        id.setEnabled(false);
+        data.setEnabled(false);
+        hora.setEnabled(false);
+        tipo.setEnabled(false);
+        valor.setEnabled(false);
+        pet.setEnabled(false);
+        cliente.setEnabled(false);
+    }
+
+    public void acaoCUD(JButton inserir, JButton cancelar, JButton deletar, JButton editar) {
+
+        inserir.setEnabled(true);
+        deletar.setEnabled(false);
+        cancelar.setEnabled(true);
+        editar.setEnabled(false);
+    }
+
+    public void acaoIrEdicao(JButton inserir, JButton cancelar, JButton deletar, JButton editar) {
+        inserir.setEnabled(false);
+        cancelar.setEnabled(false);
+        deletar.setEnabled(true);
+        editar.setEnabled(true);
+    }
+
+    public void acaoEdicao(JButton inserir, JButton cancelar, JButton deletar, JButton editar) {
+        inserir.setEnabled(true);
+        cancelar.setEnabled(true);
+        deletar.setEnabled(true);
+        editar.setEnabled(false);
+    }
+
+    public void acaoOkCancelar(JButton inserir, JButton cancelar, JButton deletar, JButton editar) {
+        inserir.setEnabled(true);
+        cancelar.setEnabled(true);
+        deletar.setEnabled(false);
+        editar.setEnabled(false);
+    }
+}
